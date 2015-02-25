bafc167de7dded2f8582b8a9aa964a05ac15a387
julienbouche <julien.bouche@gmail.com>
2015-02-25 03:46:23 +0100

    TUNING - lisibilite texte (longueur des liens)

--- index.php
+++ index.php
@@ -274,7 +274,7 @@ if(isset($_POST['username']) && isset($_POST['parentusername'])){
                     links = tree.links(nodes);
               
                 // Normalize for fixed-depth.
-                nodes.forEach(function(d) { d.y = d.depth * 150; });
+                nodes.forEach(function(d) { d.y = d.depth * 120; });
               
                 // Update the nodes…
                 var node = vis.selectAll("g.node")
