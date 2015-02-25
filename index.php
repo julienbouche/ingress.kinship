e23b819686e99c025c59e7201def7b5b6671c86d
julienbouche <julien.bouche@gmail.com>
2015-02-25 03:38:51 +0100

    REFACTO - appel calc stats dans fonction update non type (radial/linear)

--- index.php
+++ index.php
@@ -263,6 +263,9 @@ if(isset($_POST['username']) && isset($_POST['parentusername'])){
                     update_linear(source);
                 }
                 else update_radial(source);
+                
+                //recalcul des stats
+                calc_stats(root);
             }
             
             function update_radial(source) {
@@ -351,10 +354,6 @@ if(isset($_POST['username']) && isset($_POST['parentusername'])){
                   d.x0 = d.x;
                   d.y0 = d.y;
                 });
-                
-                
-                //recalcul des stats
-                calc_stats(root);
               }
 
             
@@ -447,10 +446,6 @@ if(isset($_POST['username']) && isset($_POST['parentusername'])){
                 d.x0 = d.x;
                 d.y0 = d.y;
               });
-              
-              
-                //recalcul des stats
-                calc_stats(root);
             }
             
             
